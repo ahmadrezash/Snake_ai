fbc8242787702396ee25c02d266b3c1ea86eb2f2
ahmad.sharif.abc <ahmad.sharif.abc@gmail.com>
2019-12-31 14:45:37 +0330

    get child for World

--- server_less/Snake.py
+++ server_less/Snake.py
@@ -33,6 +33,7 @@ class Snake:
 	last_action = None
 	method_name = ''
 	world = None
+	parent = None
 
 	def method(self):
 		pass
@@ -135,8 +136,8 @@ class Snake:
 
 
 def on_grid_random():
-	x = random.randint(3, e.dim//e.scale)
-	y = random.randint(3, e.dim//e.scale)
+	x = random.randint(3, e.dim // e.scale)
+	y = random.randint(3, e.dim // e.scale)
 
 	return (x * e.scale, y * e.scale)
 
@@ -154,10 +155,23 @@ class World:
 
 		self.current_snake = self.snakes[0]
 
-	def next_step(self):
-		snake = copy.deepcopy(self.current_snake)
-		# Snake Thinking
-		dir = snake.method(snake)
+	def next_step(self, dir, get_child=False):
+		if get_child:
+			### For Algorithms
+			child = []
+			for i in e.dirs:
+				world = copy.deepcopy(self)
+				if world.next_step(dir=i):
+					world.paent = self
+					child.append(world)
+			return child
+
+		if not dir:
+			### For Automatic handling
+			snake = copy.deepcopy(self.current_snake)
+			# Snake Thinking
+			dir = snake.method(snake)
+
 		self.current_snake.go_next(dir)
 		index = (self.snakes.index(self.current_snake) + 1) % len(self.snakes)
 		self.current_snake = self.snakes[index]
@@ -165,59 +179,61 @@ class World:
 		if snake.check():
 			return False
 
-	def score_check(self):
-		for i in self.snakes:
-			if i.score >= e.score:
-				return True
-		return False
 
-	def refresh_screen(self, screen, font):
-		screen.fill((0, 0, 0))
+def score_check(self):
+	for i in self.snakes:
+		if i.score >= e.score:
+			return True
+	return False
 
-		# Line
-		for x in range(0, e.dim, e.scale):  # Draw vertical lines
-			pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, e.dim))
-		for y in range(0, e.dim, e.scale):  # Draw vertical lines
-			pygame.draw.line(screen, (40, 40, 40), (0, y), (e.dim, y))
 
-		# Food Board
-		board = self.board.tolist()
-		for i in range(len(board)):
-			for j in range(len(board[0])):
-				food_font = font.render(f'{(board[i][j])}', True, (100, 100, 100))
-				food_rect = food_font.get_rect()
-				food_rect.topleft = (i * e.scale, j * e.scale)
-				screen.blit(food_font, food_rect)
+def refresh_screen(self, screen, font):
+	screen.fill((0, 0, 0))
 
-		dis = 0
-		for i in self.snakes:
-			score_font = font.render('Score: %s' % (i.score), True, (255, 255, 255))
-			score_rect = score_font.get_rect()
-			score_rect.topleft = (e.dim - 120, 10 + dis)
-			screen.blit(score_font, score_rect)
+	# Line
+	for x in range(0, e.dim, e.scale):  # Draw vertical lines
+		pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, e.dim))
+	for y in range(0, e.dim, e.scale):  # Draw vertical lines
+		pygame.draw.line(screen, (40, 40, 40), (0, y), (e.dim, y))
 
-			energy_font = font.render('Energy: %s' % (i.snake_energy), True, (255, 255, 255))
-			energy_rect = score_font.get_rect()
-			energy_rect.topleft = (80, 10 + dis)
-			screen.blit(energy_font, energy_rect)
+	# Food Board
+	board = self.board.tolist()
+	for i in range(len(board)):
+		for j in range(len(board[0])):
+			food_font = font.render(f'{(board[i][j])}', True, (100, 100, 100))
+			food_rect = food_font.get_rect()
+			food_rect.topleft = (i * e.scale, j * e.scale)
+			screen.blit(food_font, food_rect)
 
-			move_font = font.render('move: %s' % (i.movement), True, (255, 255, 255))
-			move_rect = score_font.get_rect()
-			move_rect.topleft = (250, 10 + dis)
-			screen.blit(move_font, move_rect)
-			dis = dis + 50
+	dis = 0
+	for i in self.snakes:
+		score_font = font.render('Score: %s' % (i.score), True, (255, 255, 255))
+		score_rect = score_font.get_rect()
+		score_rect.topleft = (e.dim - 120, 10 + dis)
+		screen.blit(score_font, score_rect)
 
-		for s in self.snakes:
-			flag = True
-			snake_head.fill(s.snake_head)  # Gray
-			snake_skin.fill(s.snake_skin)  # White
-			for pos in s.snake:
-				if flag:
-					screen.blit(snake_head, pos)
-					flag = False
-				else:
-					screen.blit(snake_skin, pos)
+		energy_font = font.render('Energy: %s' % (i.snake_energy), True, (255, 255, 255))
+		energy_rect = score_font.get_rect()
+		energy_rect.topleft = (80, 10 + dis)
+		screen.blit(energy_font, energy_rect)
 
-		# -----------------------------------------------------------
+		move_font = font.render('move: %s' % (i.movement), True, (255, 255, 255))
+		move_rect = score_font.get_rect()
+		move_rect.topleft = (250, 10 + dis)
+		screen.blit(move_font, move_rect)
+		dis = dis + 50
 
-		pygame.display.update()
+	for s in self.snakes:
+		flag = True
+		snake_head.fill(s.snake_head)  # Gray
+		snake_skin.fill(s.snake_skin)  # White
+		for pos in s.snake:
+			if flag:
+				screen.blit(snake_head, pos)
+				flag = False
+			else:
+				screen.blit(snake_skin, pos)
+
+	# -----------------------------------------------------------
+
+	pygame.display.update()
